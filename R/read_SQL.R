f415aadb3b26308cfee347a3880410b36be5a7f9
sokolicA <andrejsokolicc@gmail.com>
2022-07-11 14:42:47 +0200

    updated read_sql to be independent of readr

--- R/read_SQL.R
+++ R/read_SQL.R
@@ -4,20 +4,39 @@
 #'
 #' @return A string containing the sql code.
 #' @export
-#' @importFrom readr read_file
 #'
 #' @examples
 #' \dontrun{
 #'  read_SQL("~file.sql")
 #' }
-read_SQL <- function(path) {
-  qry <- readr::read_file(path)
-  #remove comments \* comment *\
-  qry <- gsub("/\\*.*?\\*/", "", qry)
-  # remove the new line at the beggining
-  qry <- gsub("^(\\r\n)*", "", qry)
-  # remove everything after ;
-  qry <- gsub("(;.*)$", "", qry)
+read_sql <- function(path) {
+  # qry <- readr::read_file(path)
+  # #remove comments \* comment *\
+  # qry <- gsub("/\\*.*?\\*/", "", qry)
+  # # remove the new line at the beggining
+  # qry <- gsub("^(\\r\n)*", "", qry)
+  # # remove everything after ;
+  # qry <- gsub("(;.*)$", "", qry)
 
-  qry
+  con = file(path, "r")
+  sql <- ""
+
+  while (TRUE){
+    line <- readLines(con, n = 1)
+
+    if ( length(line) == 0 ){
+      break
+    }
+
+    line <- gsub("\\t", " ", line)
+
+    if(grepl("--",line) == TRUE){
+      line <- paste(sub("--","/*",line),"*/")
+    }
+
+    sql <- paste(sql, line)
+  }
+
+  close(con)
+  sql
 }
