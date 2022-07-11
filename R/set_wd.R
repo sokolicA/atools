f8f68aa638f65bdf8aeb5e2155c5408714777386
sokolicA <andrejsokolicc@gmail.com>
2022-07-11 13:02:58 +0200

    added set_wd function

---
+++ R/set_wd.R
@@ -0,0 +1,17 @@
+#' Set working directory
+#'
+#' @param ... String containing directory
+#' @param file_loc Whether to set working directory to document path.
+#'
+#' @return Nothing.
+#' @export
+#'
+#' @examples
+#' #set_wd()
+set_wd <- function(..., file_loc = FALSE) {
+  if (file_loc & missing(...)) {
+    setwd(dirname(rstudioapi::documentPath()))
+  } else {
+    setwd(...)
+  }
+}
