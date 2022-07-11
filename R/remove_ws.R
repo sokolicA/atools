439f310bd80a129d6927b079ad6003f9944f7558
sokolicA <andrejsokolicc@gmail.com>
2022-07-11 13:54:05 +0200

    corrected to export all methods for remove_ws

--- R/remove_ws.R
+++ R/remove_ws.R
@@ -4,7 +4,6 @@
 #' @param ... Add
 #'
 #' @return Nothing, if x is a data.table and same object as x without white space in character columns otherwise.
-#' @export
 #'
 #' @examples
 #' \dontrun{
@@ -12,20 +11,25 @@
 #' remove_ws(x) # Nothing is returned.
 #' x$test
 #' }
+#' @export
 remove_ws <- function(...) {
   UseMethod("remove_ws")
 }
+
+#' @export
 remove_ws.data.table <- function(x) {
   cols <- colnames(x)[sapply(x, is.character)]
   x[, (cols) := lapply(.SD, trimws), .SDcols = cols]
 }
 
+#' @export
 remove_ws.data.frame <- function(x) {
   cols <- colnames(x)[sapply(x, is.character)]
   x[cols] <- apply(x[cols], 2, trimws)
   x
 }
 
+#' @export
 remove_ws.character <- function(x) {
   trimws(x)
 }
